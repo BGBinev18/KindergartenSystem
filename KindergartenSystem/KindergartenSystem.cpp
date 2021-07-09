0146a2b5895cf5211ece68cc47daf8f30ea59043
Boyan Binev <BGBinev18@codingburgas.bg>
2021-07-09 20:56:07 +0300

    Creates Menu template and function which checks if the current user is admin or parent

--- KindergartenSystem/KindergartenSystem.cpp
+++ KindergartenSystem/KindergartenSystem.cpp
@@ -5,27 +5,71 @@
 
 using namespace std;
 
-string adminUsername = "admin";
-string adminPass = "adminpass";
 
+//The only admin's username
+string adminUsername = "empty";
+
+//The only admin's password
+string adminPass = "empty";
+
+//It's value is true if the current user is admin and false if the user is parent 
+bool isAdmin;
+
+
+//Parent's data
 struct PARENT
 {
     int ID;
     string parentUsername;
-    string parentPassword;
+    string parentPass;
     string firstName;
     string lastName;
     string address;
-    CHILD childData;
+    //CHILD childData;
 };
 
+//Parent's child data
 struct CHILD
 {
     string name;
-    int address;
+    int age;
 };
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////void insertUserName()
 
+//Checks if the current user is admin or not
+bool accessLevel(string username, string password)
+{
+    if (username == adminUsername && password==adminPass)
+    {
+        isAdmin = true;
+    }
+    else
+    {
+        isAdmin = false;
+    }
+
+    return isAdmin;
+}
+
+bool mainMenu(PARENT* user)
+{
+    int choice;
+    cout << endl;
+    cout << "1: Insert user." << endl;
+    cout << "0: Exit!" << endl << endl;
+    cout << "Please enter your choice: ";
+    switch (choice) 
+    {
+    case 1:
+        
+        break;
+    case 0:
+        return false;
+        break;
+    }
+    return true;
+}
 
 int main()
 {
