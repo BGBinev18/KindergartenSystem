24ab238f09b0b67d94124684dba502aa5026976d
BGBinev18 <BGBinev18@codingburgas.bg>
2021-07-09 11:02:44 +0300

    Add structures

--- KindergartenSystem/KindergartenSystem.cpp
+++ KindergartenSystem/KindergartenSystem.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <cstdio>
+#include <string>
+
+
+using namespace std;
+
+string adminUsername = "admin";
+string adminPass = "adminpass";
+
+struct PARENT
+{
+    int ID;
+    string parentUsername;
+    string parentPassword;
+    string firstName;
+    string lastName;
+    string address;
+    CHILD childData;
+};
+
+struct CHILD
+{
+    string name;
+    int address;
+};
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
 }
