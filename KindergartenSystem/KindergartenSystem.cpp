0e6076f78d681adcde706ac9f934220952edc3fc
BGBinev18 <BGBinev18@codingburgas.bg>
2021-07-12 14:44:48 +0300

    Creates function which takes parent's data Makes things more beautiful

--- KindergartenSystem/KindergartenSystem.cpp
+++ KindergartenSystem/KindergartenSystem.cpp
@@ -15,7 +15,15 @@ string adminPass = "empty";
 //It's value is true if the current user is admin and false if the user is parent 
 bool isAdmin;
 
+//It counts the administrators
 int adminCounter = 0;
+
+//Parent's child data
+struct CHILD
+{
+    string name;
+    int age;
+};
 
 //Parent's data
 struct PARENT
@@ -26,16 +34,10 @@ struct PARENT
     string firstName;
     string lastName;
     string address;
-    //CHILD childData;
+    CHILD childData;
 };
 
-//Parent's child data
-struct CHILD
-{
-    string name;
-    int age;
-};
-
+//creates administrator 
 void logInAsAdmin()
 {
     cout << "Please, enter your username: "; 
@@ -44,6 +46,8 @@ void logInAsAdmin()
     cin >> adminPass;
     adminCounter++;
 }
+
+//It must do something....
 void logInAsAdminSecondTime()
 {
     string adminUsernameCheck;
@@ -62,7 +66,6 @@ void logInAsAdminSecondTime()
     
 }
 
-
 //Checks if the current user is admin or not
 bool accessLevel(string username, string password)
 {
@@ -78,96 +81,92 @@ bool accessLevel(string username, string password)
     return isAdmin;
 }
 
-void createParentAcc(PARENT user[], int& parentCount)
+//Creates parent's profile
+void createParentProfile(PARENT user[], int& parentCounter)
 {
     cout << "\nEnter parent's Id: " << endl;
-    cin >> user[parentCount].ID;
-
-
+    cin >> user[parentCounter].ID;
 
     cout << "\nEnter parent's username: " << endl;
-    cin >> user[parentCount].parentUsername;
-
-
+    cin >> user[parentCounter].parentUsername;
 
     cout << "\nEnter parent's password: " << endl;
-    cin >> user[parentCount].parentPass;
-
-
+    cin >> user[parentCounter].parentPass;
 
     cout << "\nEnter parent's first name: " << endl;
-    cin >> user[parentCount].firstName;
-
-
+    cin >> user[parentCounter].firstName;
 
     cout << "\nEnter parent's last name: " << endl;
-    cin >> user[parentCount].lastName;
-
-
+    cin >> user[parentCounter].lastName;
 
     cout << "\nEnter parent's address: " << endl;
-    cin >> user[parentCount].address;
+    cin >> user[parentCounter].address;
 
+    cout << "\nEnter parent's child name: " << endl;
+    cin >> user[parentCounter].childData.name;
 
+    cout << "\nEnter parent's child age: " << endl;
+    cin >> user[parentCounter].childData.age;
 
-    parentCount++;
-
-
-
+    parentCounter++;
 }
 
-bool mainMenu(PARENT* user)
+bool mainMenu(PARENT* user, int& parentCounter)
 {
     int choice;
-    cout << endl;
-    cout << "1: Log in" << endl;
-    cout << "2: Insert user." << endl;
-    cout << "0: Exit!" << endl << endl;
-    cout << "Please enter your choice: "; cin >> choice;
-    switch (choice) 
+    if (adminCounter == 0 && parentCounter == 0)
     {
-    case 1:
-        int caseOneChoice;
-        if (adminCounter == 0)
-        {
-            cout << "\nThere isnt administrator loged in yet. Please, log in as administrator!" << endl;
-            logInAsAdmin();
-
-        }
-        else
-        {
-            cout<<"There is an admin you may choose between : \n1 : Log in as admin.\n2 : Log in as parent." << endl;
-            cout << "Your choice: "; cin >> caseOneChoice;
-
-            if (caseOneChoice==1)
-            {
-                logInAsAdminSecondTime();
-                accessLevel(adminUsername, adminPass);
-            }
-        }
-        
-        
-        
-        
-        break;
-    case 2:
-        
-        break;
-    case 0:
-        return false;
-        break;
+        cout << "There aren't any users currently logged!" << endl;
+        cout << "Please log in as administrator first!" << endl;
+        logInAsAdmin();
+        accessLevel(adminUsername, adminPass);
     }
-    return true;
+    else
+    {
+        cout << "\n1: Create parent profile." << endl;
+        cout << "2: Show full parents list." << endl;
+        cout << "3. Show parent by Id." << endl;
+        cout << "4. Show parent by Id." << endl;
+        //cout << "3: Switch profile." << endl;
+        cout << "0: Exit!" << endl << endl;
+        cout << "Please enter your choice: "; cin >> choice;
+        switch (choice)
+        {
+        case 1:
+            createParentProfile(user, parentCounter);
+            break;
+        /*case 3:
+            int caseOneChoice;
+            
+            cout << "\n1 : Log in as admin.\n2 : Log in as parent." << endl;
+            cout << "Your choice: "; cin >> caseOneChoice;
+            if (caseOneChoice == 1)
+            {
+               logInAsAdminSecondTime();
+               accessLevel(adminUsername, adminPass);
+            }
+            if (caseOneChoice == 2)
+            {
+
+            }
+            break;*/
+        case 0:
+            return false;
+            break;
+            
+            return true;
+        }
+    }
 }
 
 int main()
 {
     PARENT user[100];
-    int parentCount = 0;
+    int parentCounter = 0;
     bool doShowMenu = true;
     do {
-        mainMenu(user);
-        if (mainMenu(user) == false)
+        mainMenu(user, parentCounter);
+        if (mainMenu(user,parentCounter) == false)
         {
             return 0;
         }
