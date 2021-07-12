f20948c143bca8890a6ab530102d5ae8ea40b406
BGBinev18 <BGBinev18@codingburgas.bg>
2021-07-12 11:16:54 +0300

    Creates log in as admin statement

--- KindergartenSystem/KindergartenSystem.cpp
+++ KindergartenSystem/KindergartenSystem.cpp
@@ -15,6 +15,7 @@ string adminPass = "empty";
 //It's value is true if the current user is admin and false if the user is parent 
 bool isAdmin;
 
+int adminCounter = 0;
 
 //Parent's data
 struct PARENT
@@ -35,7 +36,32 @@ struct CHILD
     int age;
 };
 
-////////////////////////////////////////void insertUserName()
+void logInAsAdmin()
+{
+    cout << "Please, enter your username: "; 
+    cin>>adminUsername;
+    cout << "Please, enter your password: ";
+    cin >> adminPass;
+    adminCounter++;
+}
+void logInAsAdminSecondTime()
+{
+    string adminUsernameCheck;
+    string adminPassCheck;
+    
+    do
+    {
+        cout << "Please, enter your username: "; cin >> adminUsernameCheck;
+        cout << "Please, enter your password: "; cin >> adminPassCheck;
+        if (adminUsernameCheck != adminUsername || adminPassCheck != adminPass)
+        {
+            cout << "\nWrong input!" << endl;
+        }
+
+    } while (adminUsernameCheck != adminUsername || adminPassCheck != adminPass);
+    
+}
+
 
 //Checks if the current user is admin or not
 bool accessLevel(string username, string password)
@@ -56,13 +82,35 @@ bool mainMenu(PARENT* user)
 {
     int choice;
     cout << endl;
-    cout << "1: Insert user." << endl;
+    cout << "1: Log in" << endl;
+    cout << "2: Insert user." << endl;
     cout << "0: Exit!" << endl << endl;
-    cout << "Please enter your choice: ";
+    cout << "Please enter your choice: "; cin >> choice;
     switch (choice) 
     {
     case 1:
+        int caseOneChoice;
+        if (adminCounter == 0)
+        {
+            cout << "\nThere isnt administrator loged in yet. Please, log in as administrator!" << endl;
+            logInAsAdmin();
 
+        }
+        else
+        {
+            cout<<"There is an admin you may choose between : \n1 : Log in as admin.\n2 : Log in as parent." << endl;
+            cout << "Your choice: "; cin >> caseOneChoice;
+
+            if (caseOneChoice==1)
+            {
+                logInAsAdminSecondTime();
+                accessLevel(adminUsername, adminPass);
+            }
+        }
+        
+        
+        
+        
         break;
     case 2:
         
@@ -76,5 +124,13 @@ bool mainMenu(PARENT* user)
 
 int main()
 {
-
+    PARENT user[10];
+    bool doShowMenu = true;
+    do {
+        mainMenu(user);
+        if (mainMenu(user) == false)
+        {
+            return 0;
+        }
+    } while (doShowMenu);
 }
